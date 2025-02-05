5dc66dced65ea6139088e9319dbfe9e197ec55a3
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-05 19:52:37 +0900

    ✨ Feat: Router 설정 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+	return (
+		// TODO: layout 작업 필요
+		<main className="text-white">메인페이지</main>
+	);
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import Info from "@/pages/Info";
+import Home from "@/pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function AppRoutes() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Info />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
