61535a618910695766384d43704e6a26c5363a35
choihyerln <choi.hyehyehihi@gmail.com>
2025-06-29 17:55:52 +0900

    ➕ Chore: 설정파일 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,19 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import prettier from "eslint-plugin-prettier";  // Prettier 플러그인 추가
+import prettier from "eslint-plugin-prettier"; // Prettier 플러그인 추가
 
 export default tseslint.config(
   {
-    ignores: ['**/build/**', '**/dist/**', 'src/some/file/to/ignore.ts'],
+    ignores: ["**/dist/**", "**/build/**", "**/node_modules/**"],
   },
   tseslint.configs.recommended,
   {
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
+      "@typescript-eslint": tseslint.plugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      prettier: prettier
+      prettier,
     },
     languageOptions: {
       parser: tseslint.parser,
@@ -23,11 +23,15 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+          useTabs: false,
+        },
       ],
-      "prettier/prettier": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
-  },
+  }
 );
